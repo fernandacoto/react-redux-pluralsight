cfcc56b934a1462092c82914ae1f5574b936a1ee
Fernanda Coto <fernanda.coto@hired.com>
2020-02-17 14:53:59 -0600

    Add/edit course functionality
    using useState, useEffect

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -5,8 +5,12 @@ export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
-    case types.CREATE_COURSE:
+    case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCESS:
+      return state.map(course =>
+        course.id === action.course.id ? action.course : course
+      );
     default:
       return state;
   }
