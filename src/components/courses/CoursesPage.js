6fe138c342a2b54090f8886b038cba87cb043839
Fernanda Coto <fernanda.coto@hired.com>
2020-02-17 10:26:56 -0600

    use mapDispatchToProps

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -4,15 +4,11 @@ import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
-  // constructor(props) { Also this is not necessary since we can set state now like =>
-  //super(props);
   state = {
     course: {
       title: ""
     }
   };
-  //  this.handleChange = this.handleChange.bind(this); //it is not needed  if we use arrow functions
-  // }
 
   handleChange = event => {
     const course = { ...this.state.course, title: event.target.value };
@@ -21,7 +17,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   };
 
   render() {
@@ -44,7 +40,7 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  createCourse: PropTypes.func.isRequired,
   courses: PropTypes.array.isRequired
 };
 
@@ -53,5 +49,13 @@ function mapStateToProps(state) {
     courses: state.courses
   };
 }
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
 
-export default connect(mapStateToProps)(CoursesPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
